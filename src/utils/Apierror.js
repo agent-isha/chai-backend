da687f52dd73afe04b7614429d39829e626fc838
Isha yadav <ishayadav@Ishas-MacBook-Air.local>
2026-08-25 17:32:55 +0530

    add utility files and update backend

---
+++ src/utils/Apierror.js
@@ -0,0 +1,21 @@
+class Apierror extends Error{
+    constructor(
+        statusCode,
+        message="something went wrong",
+        errors=[],
+        statck=""
+    ){
+        super(message)
+        this.statusCode=statusCode
+        this.data=null
+        this.message=messagethis.success=false;
+        this.errors=errors
+
+        if(statck){
+            this.stack=statck
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+export {Apierror}
