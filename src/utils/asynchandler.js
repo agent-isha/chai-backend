1ea3c29b32165048490f5c8576735596421979ff
Isha yadav <ishayadav@Ishas-MacBook-Air.local>
2026-08-27 20:40:04 +0530

    fix backend setup

--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,8 +1,8 @@
 const asynchandler=(requesthandler)=>{
-    (req,res,next)=>{
+    return (req,res,next)=>{
         Promise.resolve
 (requesthandler(req,res,next)).catch((err)=>next(err))    }
 }
 
 
-export {asynhandler}
+export {asynchandler}
